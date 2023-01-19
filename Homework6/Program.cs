2a810b45b6159b1fd885ecc5ca519a4655f07152
Mansur <cybermans@example.com>
2023-01-19 23:56:33 +0300

    Homework6

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -19,6 +19,8 @@ Console.WriteLine($"Count of positive numbers is {FindCountOfPosNumbers(m)}");
 // Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
 // уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
 // задаются пользователем.
+
+// 1й вариант
 void CoorIntersectionPoint(double k1, double b1, double k2, double b2){
     double x = 0;
     double y = 0;
@@ -41,7 +43,29 @@ double k2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Input b2: ");
 double b2 = Convert.ToDouble(Console.ReadLine());
 
+
 CoorIntersectionPoint(k1, b1, k2, b2);
+// 2й вариант
+/*double FindCoorX(double k1, double b1, double k2, double b2){
+    if(k1 == k2 && b1 == b2) return -1.10101;
+    else if(k1 == k2 && b1 != b2) return 0.00101;
+    else return Math.Round((b2-b1)/(k1-k2), 1);
+}
 
+Console.Write("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
 
+double x = FindCoorX(k1, b1, k2, b2);
+if( x == -1.10101) Console.WriteLine("Прямые совпадают. Бесконечное количество точек");
+else if(x == 0.00101) Console.WriteLine("Прямые не пересекаются");
+else{
+    double y = k1*x + b1;
+    Console.WriteLine($"Абсцисса точки пересения: {x}, ордината: {y}");
+}*/
 
